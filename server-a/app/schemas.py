34942f8737848bb9bb671c9572e0105ef11289f4
Moshe <moshe@example.com>
2026-01-13 11:51:14 +0200

    add Item schema

--- server-a/app/schemas.py
+++ server-a/app/schemas.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, HttpUrl
 from pydantic.networks import IPvAnyAddress
 
-
+class Item(BaseModel):
+    ip: str
+    
 class IPModel(BaseModel):
     ip:IPvAnyAddress
 
